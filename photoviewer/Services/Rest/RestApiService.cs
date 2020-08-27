68395991d7d0e8c7993c195bcde1609e9b34cf24
Alexey Sidorov <ai.sidorov@yahoo.com>
2020-08-28 00:56:31 +0500

    Library:  1. Rename file RestApi to RestApiService.  2. Fix project file for Core and Domain.

---
+++ photoviewer/Services/Rest/RestApiService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using photoviewer.core.Handlers;
+using photoviewer.Services.Rest;
+using Refit;
+
+namespace photoviewer.Services
+{
+    public class RestApiService : IRestApiService
+    {
+        public IRestMetodsRequest Request()
+        {
+            var settings = new RefitSettings()
+            {
+                ContentSerializer = new NewtonsoftJsonContentSerializer(new JsonSerializerSettings()
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                })
+            };
+
+            var httpClient = new HttpClient(new HttpClientDiagnosticsHandler())
+            {
+                BaseAddress = new Uri(Settings.BaseUrl),
+                Timeout = new TimeSpan(0, 0, 10)
+            };
+
+            return RestService.For<IRestMetodsRequest>(httpClient, settings);
+        }
+    }
+
+    public interface IRestApiService
+    {
+        IRestMetodsRequest Request();
+    }
+}
